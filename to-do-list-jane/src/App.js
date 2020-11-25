a57e05b46a37fe4b34cecd9a07c289fed1e2500b
jdwptr <jane.dwptrdjaja@gmail.com>
2020-11-25 21:13:44 +0700

    tugasday06 reactjs

---
+++ to-do-list-jane/src/App.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import Header from './components/header.js'
+import './App.css'
+
+class App extends React.Component {
+  constructor (props) {
+    super (props)
+    this.state = { 
+      kegiatan: ['Masak', 'Olahraga', 'Tidur']
+    }
+  }
+
+  tambah = () => {
+    let input = this.refs.aktifitas.value
+    let tempList = [...this.state.kegiatan]
+    tempList.push(input)
+    this.setState({kegiatan: tempList})
+  }
+
+  hapus = () => {
+    this.state.kegiatan.splice(0, 1)
+  }
+
+  tampil = () => {
+    const result = this.state.kegiatan.map((item, index) => <tr>
+      <td key={item}>{item}</td>
+      <td>
+      <button onClick={this.hapus}>DELETE</button>
+      </td>
+    </tr>)
+      return result
+  }
+
+
+  render () {
+    return (
+      <div className='divTabel'>
+        <Header/>
+        <input ref='aktifitas' placeholder='Input your schedule' type='text' className='inputList'/>
+        <button onClick={this.tambah} className="btnAdd">ADD</button>
+
+        <table className='tabelIsi'>
+          <thead>
+            <tr>
+            <th>Activity</th>
+            <th>Action</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {this.tampil()}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ to-do-list-jane/src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './header.css'
+
+class Header extends React.Component {
+    render () {
+        return (
+        <h1 className='header'>TO DO LIST</h1>
+        )
+    }
+}
+
+export default Header
